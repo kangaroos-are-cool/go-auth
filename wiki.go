5bb1aafae05257d85c173ed8d1159304cf62b95a
Audrey M <kangaroo.queen88@gmail.com>
2020-11-17 14:24:56 -0500

    Added server.go, updated wiki

--- wiki.go
+++ wiki.go
@@ -5,28 +5,29 @@ import (
 	"io/ioutil"
 )
 
+// Page is ...
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
-func(p *Page) save() error{
-	filename := p.Title+".txt"
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
